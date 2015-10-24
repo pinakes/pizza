6e76ad48ec8875acc6d90616c9b1533d1053dc92
Francesco Fraioli <fraiolinotes@gmail.com>
2015-10-24 18:22:35 -0400

    ciao mondo

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function draw(data) {
+    console.log(data)
+	d3.select("body")
+		.append("div")
+			.attr("class", "chart")
+		.selectAll(".bar")
+		.data(data)
+		.enter()
+		.append("div")
+			.attr("class", "bar")
+			.style("width", function(d) {return d.beers * 50 + "px"})
+			.text(function(d) { return d.beers });
+}
+
+d3.csv('http://docs.google.com/spreadsheet/pub?key=1aN_-Z0lNNCcWahCyiCs5YdkmchXoviB-4ukLVDmmQNs&single=true&output=csv', draw);
